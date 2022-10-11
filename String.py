d1b363be24867ead04583ea2589e6b564982c564
Priyansh <priyanshsaxena787.com>
2022-10-11 22:17:17 +0530

    A programme to count th enumber of words and characters of a given string

---
+++ String.py
@@ -0,0 +1,7 @@
+input_str = input("Enter a string : ")
+char = 0
+for i in input_str :
+    char += 1
+wrd = len(input_str.split())
+print("Number of words are : ", wrd)
+print("Number of charaters : ", char)

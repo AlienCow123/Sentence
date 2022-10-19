54027dd46f532e8b642aa301eaf5982b4c29140e
Shubhank Saxena <saxena.shubhank.19@gmail.com>
2022-10-19 23:44:18 +0530

    Add lower

---
+++ Lower.py
@@ -0,0 +1,11 @@
+text = 'This is a regular text'
+print(text.islower())
+#output: False
+
+text = 'this is a regular text'
+print(text.islower())
+#output: True
+
+text = 'this $ 1s @ a r3gular text!'
+print(text.islower())
+#output: True
